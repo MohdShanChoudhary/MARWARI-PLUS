c058dce13e90a197cf4fae9a440d11c1efc8ca39
themohdshan <themohdshan@gmail.com>
2025-06-13 11:59:45 +0530

    Now it is working

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,13 +2,24 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-// Get all users
+// POST - Add new user
+router.post('/', async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json({ message: "User added successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to add user", details: err.message });
+  }
+});
+
+// GET - Get all users
 router.get('/', async (req, res) => {
   const users = await User.find();
   res.json(users);
 });
 
-// Delete user by ID
+// DELETE - Delete user by ID
 router.delete('/:id', async (req, res) => {
   try {
     await User.findByIdAndDelete(req.params.id);
@@ -18,7 +29,7 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-// Update user by ID
+// PUT - Update user by ID
 router.put('/:id', async (req, res) => {
   try {
     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
